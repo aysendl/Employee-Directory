43b707a3ab1bb15732db6a66c4e1bffa663ad2ff
Ayse <aysendl@gmail.com>
2020-09-07 16:57:21 -0700

    Updating the app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Directory from "./pages/Directory";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        
-      </header>
+      <Directory />
     </div>
   );
 }
---
+++ src/pages/Directory.js
@@ -0,0 +1,58 @@
+import React from "react";
+import API from "../utils/API";
+import UserTable from "../components/UserTable";
+import SearchForm from "../components/SearchForm";
+import {Container, Card} from "react-bootstrap";
+
+class Directory extends React.Component {
+  state = {
+    name: "",
+    role: "",
+    manager: "",
+    users: [],
+    search: "",
+    searchType: "name",
+    sort: "" 
+  };
+
+  updateSearch =(search)=>{
+    // console.log(search);
+    this.setState ({search});
+  }
+
+  updateSearchType =(searchType)=> {
+    this.setState({searchType});
+  }
+
+  updateSort =(sort)=> {
+    this.setState({sort});
+  }
+
+  componentDidMount() {
+    API 
+        .getUsers()
+        .then(res => {
+          console.log(res.data.results[0] );
+          this.setState ( { users: res.data.results } );
+  });
+}
+
+  render() {
+    return (
+      <Container>
+        <Card body className = "my-4">
+          <SearchForm updateSearch ={this.updateSearch} searchType={this.state.searchType} updateSearchType={this.updateSearchType}/>
+        </Card>
+        <h2>Employees</h2>
+        <UserTable 
+          users = {this.state.users}  
+          search ={this.state.search}
+          searchType = {this.state.searchType}
+          sort={this.state.sort}
+          updateSort = {this.updateSort}
+        />
+      </Container>
+    )
+  }
+}
+export default Directory;
